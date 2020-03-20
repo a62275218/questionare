7c632c8e0c84365b268b4fbff24abbfe4bd85c7e
林榕 <linrong@lilithgames.com>
2020-03-20 16:00:21 +0800

    发布

--- deploy.sh
+++ deploy.sh
@@ -4,5 +4,6 @@ npm run build
 # 进入打包文件夹准备打包
 git add .
 git commit -m "发布"
-git subtree pull --prefix=dist origin gh-pages split --rejoin
+git subtree split --rejoin --prefix=dist
+git subtree pull --prefix=dist origin gh-pages 
 git subtree push --prefix=dist origin gh-pages
